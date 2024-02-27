8dec5efe23c5ec09156003d42519b9abf96a0770
Hao Le <vinhhao2604@gmail.com>
2024-02-27 22:37:16 +0700

    add framer motion

--- src/app/couple/page.tsx
+++ src/app/couple/page.tsx
@@ -1,54 +1,58 @@
 import Image from "next/image";
 const Couple = () => {
-    return (
-        <>
-            <div id='fh5co-couple'>
-                <div className='container'>
-                    <div className='row'>
-                        <div className='col-md-8 col-md-offset-2 text-center fh5co-heading animate-box'>
-                            <h2>Thân mời!</h2>
-                            <h3>Ngày 20 tháng 03 năm 2024</h3>
-                            <h4 style={{ fontStyle: "italic" }}>
-                                (Tức ngày 11 tháng 2 năm Giáp Thìn)
-                            </h4>
-                            <p>Hân hạnh được tiếp đón</p>
-                        </div>
-                    </div>
-                    <div className='couple-wrap animate-box'>
-                        <div className='couple-half'>
-                            <div className='groom'>
-                                <Image
-                                    src='images/groom.jpg'
-                                    alt='groom'
-                                    className='img-responsive'
-                                />
-                            </div>
-                            <div className='desc-groom'>
-                                <h3>Vĩnh Hảo</h3>
-                                <p></p>
-                            </div>
-                        </div>
-                        <p className='heart text-center'>
-                            <i className='icon-heart2'></i>
-                        </p>
-                        <div className='couple-half'>
-                            <div className='bride'>
-                                <Image
-                                    src='images/bride.jpg'
-                                    alt='groom'
-                                    className='img-responsive'
-                                />
-                            </div>
-                            <div className='desc-bride'>
-                                <h3>Mai Lan</h3>
-                                <p></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div id="fh5co-couple">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
+              <h2>Thân mời!</h2>
+              <h3>Ngày 20 tháng 03 năm 2024</h3>
+              <h4 style={{ fontStyle: "italic" }}>
+                (Tức ngày 11 tháng 2 năm Giáp Thìn)
+              </h4>
+              <p>Hân hạnh được tiếp đón</p>
             </div>
-        </>
-    );
+          </div>
+          <div className="couple-wrap animate-box">
+            <div className="couple-half">
+              <div className="groom">
+                <Image
+                  width={100}
+                  height={100}
+                  src="/images/groom.jpg"
+                  alt="groom"
+                  className="img-responsive"
+                />
+              </div>
+              <div className="desc-groom">
+                <h3>Vĩnh Hảo</h3>
+                <p></p>
+              </div>
+            </div>
+            <p className="heart text-center">
+              <i className="icon-heart2"></i>
+            </p>
+            <div className="couple-half">
+              <div className="bride">
+                <Image
+                  width={100}
+                  height={100}
+                  src="/images/bride.jpg"
+                  alt="groom"
+                  className="img-responsive"
+                />
+              </div>
+              <div className="desc-bride">
+                <h3>Mai Lan</h3>
+                <p></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Couple;
--- src/app/event/page.tsx
+++ src/app/event/page.tsx
@@ -1,4 +1,46 @@
+"use client";
+import { Variants, motion } from "framer-motion";
 const Event = () => {
+  const introHeaderVariants: Variants = {
+    hide: {
+      opacity: 0,
+      y: -100,
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 1,
+      },
+    },
+  };
+
+  const showCalendarVariants: Variants = {
+    hide: {
+      opacity: 0,
+      y: 100,
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 1,
+      },
+    },
+  };
+  const showCalendar1Variants: Variants = {
+    hide: {
+      opacity: 0,
+      y: 200,
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 1,
+      },
+    },
+  };
   return (
     <div
       id="fh5co-event"
@@ -8,16 +50,26 @@ const Event = () => {
       <div className="overlay"></div>
       <div className="container">
         <div className="row">
-          <div className="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
+          <motion.div
+            className="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box"
+            initial="hide"
+            whileInView="show"
+            variants={introHeaderVariants}
+          >
             <span>Our Special Events</span>
             <h2>Wedding Events</h2>
-          </div>
+          </motion.div>
         </div>
         <div className="row">
           <div className="display-t">
             <div className="display-tc">
               <div className="col-md-10 col-md-offset-1">
-                <div className="col-md-6 col-sm-6 text-center">
+                <motion.div
+                  className="col-md-6 col-sm-6 text-center"
+                  initial="hide"
+                  whileInView="show"
+                  variants={showCalendarVariants}
+                >
                   <div className="event-wrap animate-box">
                     <h3>Lễ cưới(Nhà gái)</h3>
                     <div className="event-col">
@@ -35,8 +87,13 @@ const Event = () => {
                       Thành, Trực Ninh, Nam Định
                     </p>
                   </div>
-                </div>
-                <div className="col-md-6 col-sm-6 text-center">
+                </motion.div>
+                <motion.div
+                  className="col-md-6 col-sm-6 text-center"
+                  initial="hide"
+                  whileInView="show"
+                  variants={showCalendar1Variants}
+                >
                   <div className="event-wrap animate-box">
                     <h3>Lễ cưới(Nhà trai)</h3>
                     <div className="event-col">
@@ -54,7 +111,7 @@ const Event = () => {
                       Ngọc Lặc, Thanh Hóa
                     </p>
                   </div>
-                </div>
+                </motion.div>
               </div>
             </div>
           </div>
